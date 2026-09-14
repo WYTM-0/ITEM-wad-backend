db496daa0c24996cbb3746a0c9aa1b3037b98d42
WYTM-0 <3.wytm.3@gmail.com>
2026-09-14 20:02:14 +0700

    Fix CORS for local and Vercel

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -1,6 +1,8 @@
 let corsHeaders = {
     "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Origin": "http://localhost:5173",
+    "Access-Control-Allow-Origin": process.env.NODE_ENV === "development"
+            ? "http://localhost:5173"
+            : "https://item-wad-frontend-one.vercel.app",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Access-Control-Max-Age": "86400",
