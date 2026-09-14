53160daf07d3df90ef3ad7eb5dfba5e446c468c3
WYTM-0 <3.wytm.3@gmail.com>
2026-09-14 20:47:02 +0700

    Debug JWT verification

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,13 +2,34 @@ import jwt from "jsonwebtoken";
 import { X_HEADER_USER_ID } from "./constant";
 const JWT_SECRET = process.env.JWT_SECRET;
 
+// export function verifyJWT(req) {
+//     try {
+//         const token = req.cookies.get("token")?.value;
+//         if (!token) {
+//             return null;
+//         }
+//         const decoded = jwt.verify(token, JWT_SECRET);
+//         return decoded;
+//     } catch (err) {
+//         console.log("==>Verify Token Exception");
+//         console.log(err);
+//         return null;
+//     }
+// }
 export function verifyJWT(req) {
     try {
         const token = req.cookies.get("token")?.value;
+
+        console.log("==> Token exists:", !!token);
+
         if (!token) {
             return null;
         }
+
         const decoded = jwt.verify(token, JWT_SECRET);
+
+        console.log("==> JWT decoded:", decoded);
+
         return decoded;
     } catch (err) {
         console.log("==>Verify Token Exception");
@@ -16,7 +37,6 @@ export function verifyJWT(req) {
         return null;
     }
 }
-
 export function isAdmin(request) {
     const headers = request.headers;
     const userId = Number(headers.get(X_HEADER_USER_ID));
